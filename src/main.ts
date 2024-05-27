b20635e23a4e0a6044fad56d5fb272dde1ed8d5c
Rama <rama@Ramas-MacBook-Pro.local>
2024-05-27 16:12:22 -0300

    fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:3005',
+      'https://talentosenior.uy',
+      'https://www.talentosenior.uy'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
